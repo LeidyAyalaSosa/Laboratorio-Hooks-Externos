d7672c6b2633489e2058e58c7305035481538b18
LeidyAyalaSosa <ayalaleidy40@gmail.com>
2022-08-21 21:40:53 -0500

    swr primera parte

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import useSWR from "swr";
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
+function App() {
+  const { data, error } = useSWR(
+    "https://api.thedogapi.com/v1/breeds?limit=3",
+    "Card",
+    fetcher
+  );
+
+  if (error) return "A ocurrido un error.";
+  if (!data) return "Cargando...";
+
+  return (
+    <div>
+      <h1>{data[0].name}</h1>
+      <p>{data[0].bred_for}</p>
+      <p>{data[0].life_span}</p>
+      <p>{data[0].temperament}</p>
+      <p>{data[0].origin}</p>
+      <img src={data[0].image.url} alt={data[0].id} width="300px" />
+      <h1>{data[1].name}</h1>
+      <p>{data[1].bred_for}</p>
+      <p>{data[1].life_span}</p>
+      <p>{data[1].temperament}</p>
+      <p>{data[1].origin}</p>
+      <img src={data[1].image.url} alt={data[1].id} width="300px" />
+      <h1>{data[2].name}</h1>
+      <p>{data[2].bred_for}</p>
+      <p>{data[2].life_span}</p>
+      <p>{data[2].temperament}</p>
+      <p>{data[2].origin}</p>
+      <img src={data[2].image.url} alt={data[2].id} width="300px" />
+      <img src={data.image} alt={data.id} width="300px" />;
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Card.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div>
+      <h1>Bulldog</h1>
+      <p>Perro gordo, tierno, baboso y comelon</p>
+      <img
+        id="3"
+        src="https://th.bing.com/th/id/R.e277f517386a050b8e27cfc3936f4af2?rik=OX%2bi124rGS027A&riu=http%3a%2f%2fwallpapersdsc.net%2fwp-content%2fuploads%2f2016%2f10%2fBulldog-Photos.jpg&ehk=MxvTjJsmTrkSnoOH1Q5yCmur8ajubl8EOhtfz8fr84Q%3d&risl=&pid=ImgRaw&r=0"
+        width="500px"
+      />
+    </div>
+  );
+}
+
+export default Card;
